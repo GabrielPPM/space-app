31dfcac55ece2d0abea6520588d22dba2945c5f4
Gabriel Plinio <gabriel.machado.tblh@gmail.com>
2025-05-27 20:35:13 -0300

    fix: galeria css, imagem css

--- src/componentes/Galeria/Imagem/Imagem.jsx
+++ src/componentes/Galeria/Imagem/Imagem.jsx
@@ -1,30 +1,40 @@
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 25rem;
-    height: auto;
-    
-    img{
-        border-radius: 1rem;
-        width: 100%;
-    }
+	display: flex;
+	flex-direction: column;
+	width: 25rem;
+	height: auto;
 
-    div{
-        height: 3rem;
-        width: 100%;
-        background-color: rebeccapurple;
-    }
-`
+	img {
+		border-radius: 1rem 1rem 0rem 0rem;
+		width: 100%;
+	}
 
-const Imagem = ({ foto }) => {
-    return(
-        <StyledDiv>
-            <img src={foto}/>
-            <div></div>
-        </StyledDiv>
-    )
-}
+	.bottom-card {
+		height: 5rem;
+		width: 100%;
+		border-radius: 0rem 0rem 1rem 1rem;
+		background-color: #001634;
+		align-content: center;
+	}
+	.texto-container {
+		margin: 0 1.5rem;
+	}
+`;
+
+const Imagem = ({ nome, foto }) => {
+	return (
+		<StyledDiv>
+			<img src={foto} />
+			<div className="bottom-card">
+				<div className="texto-container">
+					<p>Nome da imagem</p>
+					<p>{foto}</p>
+				</div>
+			</div>
+		</StyledDiv>
+	);
+};
 
 export default Imagem;
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -12,19 +12,33 @@ const GaleriaStyled = styled.section`
 const SecaoFluida = styled.div`
 	display: flex;
 	flex-grow: 1;
-	flex-direction: column;
+	flex-direction: row;
 	width: 100%;
 	justify-content: space-between;
-`
+	.container-galeria {
+		width: 100%;
+	}
+	.container-imagens{
+		display: inline-flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+}
+`;
 
-const Galeria = ({fotos = []}) => {
+const Galeria = ({ fotos = [] }) => {
 	return (
 		<>
 			<Tags />
 			<GaleriaStyled>
 				<SecaoFluida>
-					<Titulo>Navegue pela galeria</Titulo>
-					{fotos.map( (foto, index) => <Imagem key={index} foto={foto.path}/>)}
+					<div className="container-galeria">
+						<Titulo>Navegue pela galeria</Titulo>
+						<div className="container-imagens">
+							{fotos.map((foto, index) => (
+								<Imagem key={index} foto={foto.path} />
+							))}
+						</div>
+					</div>
 					<Populares />
 				</SecaoFluida>
 			</GaleriaStyled>
